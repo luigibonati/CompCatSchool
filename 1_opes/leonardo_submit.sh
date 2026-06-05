f1325cfe5136e6ae39bc335b72c0c6e73dd3067a
Luigi Bonati <lui.bonati@gmail.com>
2026-06-05 22:52:33 +0200

    update opes analysis

--- 1_opes/leonardo_submit.sh
+++ 1_opes/leonardo_submit.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 SBATCH_FILE="leonardo.sbatch"
 
-for TEMP in 300 700; do
+for TEMP in 700; do
     for BIAS in OPES_METAD OPES_METAD_EXPLORE; do
 
         if [[ "$BIAS" == "OPES_METAD_EXPLORE" ]]; then
-            BARRIER=0.8
+            BARRIER=0.6
             LABEL="explore"
         else
             BARRIER=1.0
